96161e552e9e8a5a9ad267edac5a855240601b96
Konstantin Latypov <Konstantin Latypov>
2014-06-07 18:59:56 +0400

    Bugfix

--- index.js
+++ index.js
@@ -19,11 +19,15 @@ function processSVGImage(data, fileName) {
     var result = converter.convert(data);
     console.log( result );
 
+    var path = '';
+    if ( result.d != '' ) {
+        path = '<path d="' + result.d + '" />';
+    }
     if ( !result.error ) {
         var xml = '<?xml version="1.0" ?>' +
         '<!DOCTYPE svg  PUBLIC \'-//W3C//DTD SVG 1.1//EN\'  \'http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd\'>' +
         '<svg version="1.1" width="' + result.width + '" height="' + result.height + '" x="' + result.x + '" y="' + result.y + '"  xml:space="preserve" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">' +
-        '<path d="' + result.d + '" />' +
+        + path +
         '</svg>';
 
         fs.writeFile('result.svg', xml, function (err) {
