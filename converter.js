f44f1a08ebce5ff6a20d9282aab16aa0b1844b50
Konstantin Latypov <Konstantin Latypov>
2014-06-09 21:46:09 +0400

    Refactoring

--- converter.js
+++ converter.js
@@ -10,7 +10,8 @@ var _ = require('lodash');
 var SvgPath = require('svgpath');
 
 var quietTags = ['desc', 'title'];
-var quietAttributes = ['id', 'd', 'transform'];
+var quietAttrs = ['id', 'd', 'transform'];
+var supportedTags = ['path'];
 
 /**
  * Parse path tag
@@ -19,99 +20,79 @@ var quietAttributes = ['id', 'd', 'transform'];
  * @param parentTransforms
  * @returns {{d: *, transform: string, guaranteed: boolean}}
  */
-var parsePath = function ( item, parentTransforms ) {
+function  parsePath( item, parentTransforms ) {
     var transform = ( item.getAttribute('transform') ) ? parentTransforms + ' ' + item.getAttribute('transform') : parentTransforms;
     return { 'd': item.getAttribute('d'),
-                'transform': transform,
-                'guaranteed' : true};
-};
+        'transform': transform,
+        'guaranteed' : true};
+}
 
-// Key - supported tag, value - function for parse tag. See parseTags function for example
-var supportedTags = { path : parsePath };
+
 
 /**
  * Removing tags which can't be converted.
  *
- * @param xmlDoc
- * @param removed  Array with removed tags
+ * @param node
+ * @param ignoredTags  Hash with ignored tags
+ * @param ignoredAttrs Hash with ignored attributes
  * @param parentTransforms  Text with parent transforms
- * @returns {{pathData: Array of  Object with contains path, transforms,
+ * @returns {{paths: Array of  Object with contains path, transforms,
  *              removed: Array, guaranteed: boolean}}
  */
-function removeTags (xmlDoc, removed, parentTransforms ) {
-    var pathData = [];
+function processTree(node, ignoredTags, ignoredAttrs, parentTransforms ) {
+    var paths = [];
     var guaranteed = true;
-    _.each( xmlDoc.childNodes, function( item )  {
+    _.each( node.childNodes, function( item )  {
+        //TODO: Check nessasary
         if ( item.nodeType !== 1 ) {
             return;
         }
+        // Quiet ignored tags
+        if ( quietTags.indexOf( item.nodeName ) > -1) {
+            return;
+        }
+
         // Parse nested g tags
-        if ( item.childNodes.length > 0 && item.nodeName === 'g' ) {
+        if ( item.nodeName === 'g' ) {
             var transforms = ( item.getAttribute('transform') ) ? parentTransforms + ' ' + item.getAttribute('transform') : parentTransforms;
-            var result = removeTags( item, removed, transforms);
-            pathData = pathData.concat( result.pathData );
+            var result = processTree( item, ignoredTags, ignoredAttrs, transforms);
+            paths = paths.concat( result.paths );
+            ignoredTags = ignoredTags.concat( result.ignoredTags );  //FIXME: Bugfix
+            ignoredAttrs = ignoredAttrs.concat( result.ignoredAttrs );
             guaranteed = guaranteed && result.guaranteed ;
         }
 
         // Remove not supported tags
-        if ( !_.has( supportedTags, item.nodeName ) &&
-                _.indexOf( removed, item.nodeName ) === -1 &&
-                _.indexOf( quietTags, item.nodeName ) ) {
-            removed.push( item.nodeName );
+        if ( supportedTags.indexOf( item.nodeName ) < 0) {
+            ignoredTags[ item.nodeName ] = true;
             return;
         }
 
         // Parse supported tag
-        var res = supportedTags[ item.nodeName ] ( item, parentTransforms );  // Calls function for supported tag
-        pathData.push( { 'd': res.d, 'transform': res.transform} );
+        var res = {};
+        switch ( item.nodeName ) {
+            case 'path' : res = parsePath( item, parentTransforms );
+                            break;
+        }
+
+        var transformedPath = new SvgPath( res.d ).transform( res.transform ).toString();
+        paths.push( transformedPath );
         guaranteed = guaranteed && res.guaranteed;
 
         // Remove not supported attributes
         _.each(item.attributes, function (item) {
-            if (_.indexOf(removed, item.nodeName) === -1 && _.indexOf(quietAttributes, item.nodeName) === -1) {
-                removed.push(item.nodeName);
+            if (quietAttrs.indexOf( item.nodeName) === -1) {
+                ignoredAttrs[item.nodeName = true;
             }
         });
     });
 
     return {
-        pathData: pathData,
-        removed: removed,
+        paths: paths,
+        ignoredTags: ignoredTags,
+        ignoredAttrs : ignoredAttrs,
         guaranteed: guaranteed
     };
-}
-
-/**
- * Merge transforms
- *
- * @param pathData
- * @returns {Array} of paths
- */
-function mergeTransforms( pathData ) {
-    var paths = [];
-    _.each( pathData, function( item )  {
-        if ( !item.transform || item.transform === '' ) {
-            paths.push( item.d );
-            return;
-        }
-        var transformedPath = new SvgPath( item.d ).transform( item.transform ).toString();
-        paths.push( transformedPath );
-    });
-    return paths;
-}
-
-/**
- * Merge paths from array to string
- *
- * @param paths
- * @returns {string}
- */
-function mergePaths( paths ) {
-    var result = '';
-    _.each( paths, function( item ) {
-        result += item;
-    });
-    return result;
 }
 
 /**
@@ -143,19 +124,11 @@ function getCoordinates(svg) {
  *
  *
  * @param xml
- * @returns {{d: string,
- *              width: number,
- *              height: number,
- *              x: number,
- *              y: number,
- *              removedTags: Array,
- *              error: Error,
- *              guaranteed: boolean}}
+ * @returns {{d: "", width: number, height: number, x: number, y: number, ignoredTagsTags: Array, ignoredAttrs: Array, error: null, guaranteed: boolean}}
  */
 function convert( sourceXml ) {
     console.log('Convert data\n');
 
-    var guaranteed = true;
     var error = null;
     var ans = {
         d : '',
@@ -163,7 +136,8 @@ function convert( sourceXml ) {
         height : 0,
         x : 0,
         y : 0,
-        removedTags : [],
+        ignoredTagsTags : [],
+        ignoredAttrs : [],
         error : error,
         guaranteed : false
     };
@@ -180,33 +154,32 @@ function convert( sourceXml ) {
     })).parseFromString( sourceXml , 'application/xml');
 
     if ( error ) {
+        ans.error = error;
         return ans;
     }
 
     var svg = xmlDoc.getElementsByTagName('svg')[0];
 
-    var result = removeTags( svg, [], '' );
-/*    _.each( result.pathData, function ( item ) {
-        console.log( item.d + " - " + item.transform );
-    });*/
-    guaranteed = result.guaranteed;
+    var result = processTree( svg, {}, {}, '' );
+    var guaranteed = result.guaranteed;
 
-    var removedTags = result.removed;
-
-    var paths = mergeTransforms( result.pathData );
-
-    result = mergePaths( paths );
-    if ( paths.length > 0) {
+    var mergedPath = '';
+    _.each( result.paths, function( item ) {
+        mergedPath += item;
+    });
+    if ( result.paths.length > 0) {
         guaranteed = false;
     }
+
     var coords = getCoordinates( svg );
 
-    ans.d  = result;
+    ans.d  = mergedPath;
     ans.width = coords.width;
     ans.height = coords.height;
     ans.x = coords.x;
     ans.y = coords.y;
-    ans.removedTags = removedTags;
+    ans.ignoredTags = result.ignoredTags;
+    ans.ignoredAttrs = result.ignoredAttrs;
     ans.guaranteed = guaranteed;
     return ans;
 }
